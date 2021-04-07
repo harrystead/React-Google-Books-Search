9212692c88d5664e02740bd69ae8e28dc6f3eb0e
Harry Stead <harrystead@Harrys-MacBook-Air.local>
2021-04-07 11:13:38 +0100

    navbar with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Search from "./pages/Search";
+import Save from "./pages/Save";
+import NavbarToggle from "./components/Nav"; 
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+    <NavbarToggle/>
+    <Route exact path="/" component={Search} />
+    <Route path="/edit" component={Save} />
+  </Router>
   );
 }
 
---
+++ client/src/components/Nav.js
@@ -0,0 +1,18 @@
+
+import { Link } from 'react-router-dom';
+import { Navbar, Nav } from "react-bootstrap";
+
+const NavbarToggle = () => {
+    return (
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="/">Google Books</Navbar.Brand>
+  <Navbar />
+    <Nav className="mr-auto">
+      <Link to="/" className="nav-link">Search</Link>
+      <Link to="/save" className="nav-link">Save</Link>
+    </Nav>
+</Navbar>
+    )
+}
+
+export default NavbarToggle;
