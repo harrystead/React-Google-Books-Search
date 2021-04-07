bb97e81cf5990c5679f57d95eb4f110493af92a0
Harry Stead <harrystead@Harrys-MacBook-Air.local>
2021-04-07 14:28:54 +0100

    card list added

---
+++ client/src/components/Cards/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Jumbotron from "../Jumbotron";
+import CardList from "../Card";
+
+export default function Cards() {
+    return (
+        <div>
+        <CardList />
+        </div>
+    )
+}
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Intro from "../components/Intro"
+import Intro from "../components/Intro";
+import Cards from "../components/Cards";
 
 export default function Search() {
     return (
         <div>
             <Intro />
+            <Cards />
         </div>
     )
 }
