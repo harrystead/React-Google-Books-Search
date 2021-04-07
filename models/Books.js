ba824a1176ba080590a1296429197317df491021
Harry Stead <harrystead@Harrys-MacBook-Air.local>
2021-04-07 11:37:23 +0100

    book model

---
+++ models/Books.js
@@ -0,0 +1,16 @@
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const booksSchema = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    link: { type: String },
+    date: { type: Date, default: Date.now }
+  });
+  
+  const Books = mongoose.model('Books', booksSchema);
+  module.exports = Books;
