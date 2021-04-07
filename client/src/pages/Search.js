7b0dead4978b38924b1a119ada92826a6f00220c
Harry Stead <harrystead@Harrys-MacBook-Air.local>
2021-04-07 13:20:29 +0100

    utils for api operations

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
 
 export default function Search() {
     return (
         <div>
-            <h2>hello</h2>
+            
         </div>
     )
 }
