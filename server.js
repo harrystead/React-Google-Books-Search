c386fa0e34b9c04161c27ebdf38547ac86a60bd4
Harry Stead <harrystead@Harrys-MacBook-Air.local>
2021-04-09 17:47:25 +0100

    last touches

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(routes);
-mongoose.connect(
-  "mongodb+srv://harry123:harryjack1214@cluster0.7trlh.mongodb.net/Library?retryWrites=true&w=majority",
+mongoose.connect(MONGODB_URL,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 mongoose.connection.once("open", () => {
